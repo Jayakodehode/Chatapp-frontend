c1ee541fc8628405ec1176cbac16468ae44ef10f
Jayakodehode <jaya.kodehode@gmail.com>
2023-10-06 14:14:00 +0200

    github pages

--- public/Chatapp/src/App.jsx
+++ public/Chatapp/src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./Register";
+import Login from "./Login";
+import Chat from "./Chat";
 
 import "./App.css";
 import MainLayout from "./pages/Mainlayout";
@@ -7,7 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Chat-app/*" element={<MainLayout />} />
+        <Route path=" /Chat-app/register" element={<Register />} />
+        <Route path=" /Chat-app/login" element={<Login />} />
+        <Route path=" /Chat-app/" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
