8aad112790ddad70008fceb9689e93e4359601d5
Jayakodehode <jaya.kodehode@gmail.com>
2023-10-27 13:42:27 +0200

    chaged host to render link

--- public/Chatapp/src/utils/APIRoute.js
+++ public/Chatapp/src/utils/APIRoute.js
@@ -1,7 +1,7 @@
-export const host = "http://localhost:5000";
+export const host = "https://chat-app-frontend-igyk.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
-export const allUsersRoute = `${host}/api/auth/allUsers`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
 export const sendMessageRoute = `${host}/api/messages/addmessage`;
 export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
